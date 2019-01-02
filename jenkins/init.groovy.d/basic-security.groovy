9966c8841e29ec74dc4a65d649ff14a884f82069
JGuzman4 <jeffg@liatrio.com>
2019-01-02 14:58:32 -0800

    adding seed job and plugins from alexs branch

--- jenkins/init.groovy.d/basic-security.groovy
+++ jenkins/init.groovy.d/basic-security.groovy
@@ -1,5 +1,6 @@
 import jenkins.model.*
 import hudson.security.*
+import hudson.security.csrf.DefaultCrumbIssuer
 
 def instance = Jenkins.getInstance()
 
@@ -7,9 +8,12 @@ def instance = Jenkins.getInstance()
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount('admin','admin')
 instance.setSecurityRealm(hudsonRealm)
+instance.setCrumbIssuer(new DefaultCrumbIssuer(true))
 instance.save()
 
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
 instance.setAuthorizationStrategy(strategy)
+
 instance.save()
 
